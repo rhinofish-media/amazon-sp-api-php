5f95d6d8df32a545e9b936f8f725dcf3889643dc
Sharat Hegde <sharat@inika.com>
2022-02-02 09:34:26 +0530

    Fixes to parent::listInvalidProperties

--- lib/Models/Orders/OrderItemBuyerInfoList.php
+++ lib/Models/Orders/OrderItemBuyerInfoList.php
@@ -167,7 +167,7 @@ class OrderItemBuyerInfoList implements ModelInterface, ArrayAccess, IterableTyp
      */
     public function listInvalidProperties()
     {
-        $invalidProperties = parent::listInvalidProperties();
+        $invalidProperties = ModelInterface::listInvalidProperties();
 
         return $invalidProperties;
     }
--- lib/Models/Orders/OrderList.php
+++ lib/Models/Orders/OrderList.php
@@ -167,7 +167,7 @@ class OrderList implements ModelInterface, ArrayAccess, IterableType
      */
     public function listInvalidProperties()
     {
-        $invalidProperties = parent::listInvalidProperties();
+        $invalidProperties = ModelInterface::listInvalidProperties();
 
         return $invalidProperties;
     }
